f1525786a432655b273265b651010cf3dd4fc0a5
Ubuntu <ubuntu@ip-172-31-20-220.ap-southeast-1.compute.internal>
2019-11-29 06:01:28 +0000

    Added calculator answer

---
+++ src/Calculator.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+export default class Calculator extends React.Component {
+    
+    state = {
+        number1:'0',
+        number2:'0',
+        operation:'add', // add, subtract, multiply, divide
+        results:0,
+        buttonPressed:false
+    }
+    
+    handleChange = (event) =>
+    {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    }
+    
+    calculate = () => {
+        let x = parseFloat(this.state.number1);
+        let y = parseFloat(this.state.number2);
+        let results = 0
+        
+        if (this.state.operation==='add') {
+            results = x + y;
+        } else if (this.state.operation == 'subtract') {
+            results = x -y;
+        } else if (this.state.operation == 'multiply') {
+            results = x * y;
+        } else if (this.state.operation == 'divide') {
+            results = x / y;
+        }
+        
+        // this.setState({
+        //     results:results
+        // });
+        
+        this.setState({
+            results, // same as: results : results
+            buttonPressed:true
+        })
+        
+    }
+
+    render(){
+        return (
+            <div>
+                <input type='text' name='number1' value={this.state.number1} onChange={this.handleChange}/> <br/>
+                <input type='text' name='number2' value={this.state.number2} onChange={this.handleChange}/>
+                <div>
+                    <input type='radio' name='operation' value='add' checked={this.state.operation==='add'} onChange={this.handleChange}/>Add
+                    <input type='radio' name='operation' value='subtract' checked={this.state.operation==='subtract'} onChange={this.handleChange}/>Subtract
+                    <input type='radio' name='operation' value='multiply' checked={this.state.operation==='multiply'} onChange={this.handleChange}/>Multiply
+                    <input type='radio' name='operation' value='divide' checked={this.state.operation==='divide'} onChange={this.handleChange}/>Divide
+                </div>
+                <div>
+                    <button onClick={this.calculate}>Calculate</button>
+                </div>
+                {this.state.buttonPressed ? this.state.results  : null}
+            </div>
+        )
+    }
+}
+
